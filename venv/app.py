7236749c3f5ecff04737fc09012a86c765b75bea
Aman2221 <as562770@gmail.com>
2022-11-14 21:46:33 +0530

    trying deployment

--- venv/app.py
+++ venv/app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 
+st.set_page_config(
+    page_title='Salary Predictor',
+    page_icon='asset/logo.png'
+)
 
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 

ccb32dbe25e84473e42c7dc7782f07e83a9fb9d9
Aman-Fenixwork <aman@fenixwork.com>
2023-04-14 11:41:27 +0530

    sign up added

--- venv/login_page.py
+++ venv/login_page.py
@@ -1,6 +1,18 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import firebase_admin
+from firebase_admin import db
+from firebase_admin import auth
+from firebase_admin import credentials
+
+cred = credentials.Certificate("./firebase-adminsdk.json")
+
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://console.firebase.google.com/project/msc-it-python/database'
+})
+
+
 # from firebase_admin import firestore
 
 # firebase_config = {
@@ -27,6 +39,16 @@ def login():
         if isLogin == True:
             if len(email) == 0 or len(password) == 0:
                 st.warning('Please fill all fields properly', icon="⚠️")
+            else:
+                try:
+                    user = auth.get_user_by_email(email)
+                    st.write("user :", user)
+                    auth.sign_in_with_email_and_password(email, password)
+                    st.success('User logged in successfully',icon="✅")
+                except Exception as e:
+                    st.warning('Error while logging in user'.format(e))
+                    st.write("Error :",e)
+
     with tab2:
         name = st.text_input("Name")
         new_email = st.text_input("Email", key="Your email")
@@ -34,8 +56,18 @@ def login():
             "Password", key="Your password",  type="password")
         isSignup = st.button("Sign up", type="primary")
         if isSignup == True:
-            if len(name) == 0 or len(new_email) == 0 or len(new_password):
+            if len(name) == 0 or len(new_email) == 0 or len(new_password) == 0:
                 st.warning('Please fill all fields properly', icon="⚠️")
+            else :
+                try:
+                    user = auth.create_user(
+                        email=new_email,
+                        password=new_password
+                    )
+                    st.success('User created successfully',icon="✅")
+                except Exception as e:
+                    st.warning('Error creating user:'.format(e))
+
     # with col1:
     #     isLogin = st.button("Login")
 
